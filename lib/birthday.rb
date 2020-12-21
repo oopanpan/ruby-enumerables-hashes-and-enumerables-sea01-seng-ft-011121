92dd02a20c8c8252bd2fd16f08cea61a101152c9
Kai Peng Li <oopanpan@gmail.com>
2020-12-21 09:29:18 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,4 +6,9 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
+  birthday_kids.each{ |kiddo, age|
+    #puts "Happy Birthday " + kiddo + "! You are now " + age.to_s + " years old!"
+  #}
+    puts "Happy Birthday #{kiddo}! You are now #{age} years old!"
+  }
 end
