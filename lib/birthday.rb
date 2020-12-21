c546f00f8a0fb0c0021d17bd096088935d7010b9
Kai Peng Li <oopanpan@gmail.com>
2020-12-21 09:27:52 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,6 +7,6 @@
 def happy_birthday(birthday_kids)
   # add your code snippet here!
   birthday_kids.each{ |kiddo, age|
-    puts "Happy Birthday " + kiddo + "! You are now " + age + " years old!"
+    puts "Happy Birthday " + kiddo + "! You are now " + age.to_s + " years old!"
   }
 end
